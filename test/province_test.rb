cd9b07b5481261521cfab9907d3fddd1daccdcbd
priscillashort <pshort2@cocc.edu>
2021-05-13 16:32:11 -0700

    test the results of a zero demand with all other defaults

--- test/province_test.rb
+++ test/province_test.rb
@@ -23,6 +23,12 @@ class ProvinceTest < Minitest::Test
       assert_equal(-6, @asia.shortfall)
       assert_equal(292, @asia.profit)
     end
+
+    def test_province_zero_demand
+      @asia.demand = 0
+      assert_equal(-25, @asia.shortfall)
+      assert_equal(0, @asia.profit)
+    end
   end
 
   describe 'no producers' do
